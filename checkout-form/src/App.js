51ae54f79f6ba5cacd28de7fa09f2e52a11df07b
StClairJohnson <StClair.Johnson@cdpipelinedevshop.com>
2022-07-25 14:12:35 +0000

    checkout-form

---
+++ checkout-form/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './App.css';
+import Checkout from "./Components/Checkout"
+
+function App() {
+  return (
+    <div className="App" style={{ 
+      backgroundImage: `url("https://wallpaperaccess.com/full/225661.jpg")` 
+    }}>
+      <Checkout></Checkout>
+    </div>
+  );
+}
+
+export default App;
